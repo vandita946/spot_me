241619e5d1277ce7004a8ca3ad59b1c3443964b9
joel chen <joelchenjg@gmail.com>
2020-07-07 16:45:43 +0800

    update with errors

--- app/policies/goal_policy.rb
+++ app/policies/goal_policy.rb
@@ -6,6 +6,18 @@ class GoalPolicy < ApplicationPolicy
   end
 
   def show?
-    reuturn true
+    return true
   end
+
+  def update?
+    record.user == user
+    # For documentation purposes - fact check:
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+
 end
