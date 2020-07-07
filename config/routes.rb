0288fb189cf2ca35e799167e960250885f32ee4a
Vandita Attal <vandita.attal@gmail.com>
2020-07-07 17:11:32 +0800

    milestone controller methods added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :goals do
+    resources :milestones
+  end
+
+  resources :milestones do
+    resources :completion_message, only: [:new, :create]
+  end
 end
