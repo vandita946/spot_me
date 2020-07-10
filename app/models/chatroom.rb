8200cc51b19b3abe7235f1d140ffe76289824e1e
joel chen <joelchenjg@gmail.com>
2020-07-10 15:39:47 +0800

    update to previous commit

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,5 @@
 class Chatroom < ApplicationRecord
   belongs_to :topic, polymorphic: true, dependent: :destroy
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :owner_connections, :class_name => 'Connection', :foreign_key => 'owner_id'
   has_many :buddy_connections, :class_name => 'Connection', :foreign_key => 'buddy_id'
-  has_many :goals
+  has_many :goals, dependent: :destroy
   has_many :messages
   has_many :connections, through: :owner_connections, source: :buddy
   has_many :fans, through: :buddy_connections, source: :owner
