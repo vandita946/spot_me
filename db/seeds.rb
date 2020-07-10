4fa1a519eb6421e5a59e261162fc2cb62be24601
joel chen <joelchenjg@gmail.com>
2020-07-10 11:45:53 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Message.destroy_all
 Chatroom.destroy_all
 GoalConnection.destroy_all
 Connection.destroy_all
