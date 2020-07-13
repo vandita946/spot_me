31c88cf6af0ad51da27288a2316f95e406e7438d
Claire06140 <claire.marchyllie25@gmail.com>
2020-07-13 13:13:53 +0800

    first version of feed with milestone completions

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,7 +3,25 @@ class FeedsController < ApplicationController
     @user = current_user
     @goals = @user.goals
 
-    @feeds = (@goals.milestones).sort_by {|a| a.deadline}.reverse
+    # create array with all users milestones
+    # sort i
+    @user_milestones = []
+    @goals.each do |goal|
+      goal.milestones.each do |milestone|
+        @user_milestones << milestone
+      end
+    end
 
-  end
+    @sorted_milestones = @user_milestones.sort_by(&:updated_at).reverse
+    end
+
+
+    # @feeds = @goals.milestones.sort_by {|a| a.deadline}.reverse
+
 end
+
+
+def get_latest
+    incomplete = self.milestones.where(is_completed: false)
+    sorted = incomplete.sort_by(&:deadline).first
+  end
